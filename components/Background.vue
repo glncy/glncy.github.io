125c49584db923721ef85fbaddf7f24414ecdf7d
Glency Tirao <glency.a.tirao@gmail.com>
2022-07-27 19:40:59 +0800

    Initial Commit

---
+++ components/Background.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="background wrap"></div>
+</template>
+
+<style lang="postcss" scoped>
+.background {
+  @apply w-full h-full;
+
+  &.wrap {
+    @apply bg-black;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <Background/>
+  </div>
+</template>
+<style lang="postcss" scoped>
+.wrapper {
+  @apply w-screen h-screen;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
