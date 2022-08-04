665dd348e7ee4d3367991b3b124a984dff49e224
Glency Tirao <glnse@glnse.local>
2022-08-04 23:34:23 +0800

    Initialize Icons

---
+++ components/IconGlassWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="icon-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.icon-wrapper {
+  @apply rounded-full h-24 w-24 flex items-center justify-center;
+  background: rgba(255, 255, 255, 0.08);
+  backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+
+  > i {
+    @apply text-5xl;
+  }
+}
+</style>
---
+++ components/sections/about/SkillsSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-wrapper">
+    <div class="section">
+      <IconGlassWrapper>
+        <i class="devicon-javascript-plain colored"/>
+      </IconGlassWrapper>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.section {
+  @apply flex flex-col w-full h-screen items-center justify-center;
+}
+</style>
+
+<script lang="ts">
+import IconGlassWrapper from "~/components/IconGlassWrapper.vue";
+export default { components: { IconGlassWrapper } }
+</script>
